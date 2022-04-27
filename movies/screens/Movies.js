1c6afe82eb67f230c8c07032faffd2b0b43fa4bc
syo0e <kimdlzp@gmail.com>
2022-04-27 16:52:38 +0900

    Themes

--- movies/screens/Movies.js
+++ movies/screens/Movies.js
@@ -2,19 +2,20 @@ import React from "react";
 import styled from "styled-components/native";
 
 const Btn = styled.TouchableOpacity`
-  flex: 1,
-  justify-contner: center;
+  flex: 1;
+  justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.mainBgColor};
 `;
 
 const Title = styled.Text`
-  color: ${(props) => (props.selected ? "blue" : "red")};
+  color: ${(props) => props.theme.textColor};
 `;
 
 const Movies = ({ navigation: { navigate } }) => (
   <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
-    <Title selected={false}>Movies</Title>
-    <Title selected={true}>Movies</Title>
+    <Title>Movies</Title>
   </Btn>
 );
+
 export default Movies;
